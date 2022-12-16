8b4d64fa3eec8903028a53d5d63ccdc5034686ea
Davi-Ga <davigaldinoky@gmail.com>
2022-12-16 08:39:49 -0300

    if

--- assyncjs/worker.js
+++ assyncjs/worker.js
@@ -1,27 +1,23 @@
 //Luta de x1 de habilidade característica
 self.onmessage = (element) =>{
-    const aView = new Int32Array(element.data.v);
-    const fView = new Int32Array(element.data.f);
+    const aV = new Int32Array(element.data.v);
+    const fV = new Int32Array(element.data.f);
 
-    let duel = Math.ceil(Math.random()*49);
+    let duel = Math.ceil(Math.random()*10);
 
     while(true){
         tibiano = Math.ceil(Math.random()*50);
         tibiano2 = Math.ceil(Math.random()*50);
 
-        while(tibiano1==tibiano2){
-            tibiano1=Math.ceil(Math.random()*50);
-           
-        }
         if (tibiano1['value']>tibiano2['value']){
-            Atomics.wait(fView, 0, 0);
-            Atomics.store(fView, 0, 0);
+            Atomics.wait(fV, 0, 0);
+            Atomics.store(fV, 0, 0);
 
-            Atomics.store(aView, duel, tibiano1['value']);
-            Atomics.store(aView, duel+1, tibiano1['value']);
+            Atomics.store(aV, duel, tibiano1['value']);
+            Atomics.store(aV, duel+1, tibiano1['value']);
 
         }
-        Atomics.store(fView, 0, 1);
-        Atomics.notify(fView, 0, 1);
+        Atomics.store(fV, 0, 1);
+        Atomics.notify(fV, 0, 1);
     }
 }
